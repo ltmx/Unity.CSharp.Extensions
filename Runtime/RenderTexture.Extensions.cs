266f09cf650fc9f22cf2b7578dc923af64d1feb8
LTMX <47640688+LTMX@users.noreply.github.com>
2024-02-15 16:18:32 +0000

    Added RenderTexture.Extensions

---
+++ Runtime/RenderTexture.Extensions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public static class RenderTextureExtensions
+{
+    public static void CopyFrom(this Texture destination, Texture source) => Graphics.CopyTexture(source, destination);
+    public static void CopyTo(this Texture source, Texture destination) => Graphics.CopyTexture(source, destination);
+    
+    public static void SetUseMipMaps(this RenderTexture renderTexture, bool useMips)
+    {
+        if (useMips == renderTexture.useMipMap) return;
+        renderTexture.Release();
+        renderTexture.useMipMap = useMips;
+        renderTexture.Create();
+
+    }
+        
+        
+}
