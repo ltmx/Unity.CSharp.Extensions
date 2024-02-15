77d716fe4bf5b12f70e2e64dc852c2ae4bb2b77b
LTMX <47640688+LTMX@users.noreply.github.com>
2024-02-15 15:19:34 +0000

    Added Vector3 Extensions

---
+++ Runtime/Vector3Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Unity.Mathematics;
+using UnityEngine;
+
+public static class Vector3Extensions
+{
+    public static Vector4 ToVector4(this Vector3 v) => new(v.x, v.y, v.z, 1);
+    public static float4 ToFloat4(this float3 v) => new(v, 1);
+
+    public static List<Vector4> ToVector4List(this IEnumerable<Vector3> list) => list.Select(ToVector4).ToList();
+
+    public static List<float4> ToFloat4List(this IEnumerable<float3> list) => list.Select(ToFloat4).ToList();
+}
