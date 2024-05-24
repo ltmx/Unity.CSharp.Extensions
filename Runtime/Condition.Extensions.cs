b2cfd42ab2fde485bf8b281ad01fc379862e8866
LTMX <47640688+LTMX@users.noreply.github.com>
2024-05-24 15:43:20 +0100

    Added to Condition.Extensions OrNot() & xml documentation

--- Runtime/Condition.Extensions.cs
+++ Runtime/Condition.Extensions.cs
@@ -10,41 +10,57 @@ using System;
 
 public static class ConditionExtensions {
     
+    // Boolean Logic
+    /// <summary> Logical a AND b</summary>
     public static bool And(this bool a, bool b) => a && b;
+    /// <summary> Logical a AND NOT b</summary>
     public static bool AndNot(this bool a, bool b) => a && !b;
+    /// <summary> Logical NAND (not a and not b</summary>
     public static bool Nand(this bool a, bool b) => !a && !b;
+    /// <summary> Logical a OR b</summary>
     public static bool Or(this bool a, bool b) => a || b;
-    public static bool OrNot(this bool a, bool b) => a || b;
+    /// <summary> Logical a OR NOT b </summary>
+    public static bool OrNot(this bool a, bool b) => a || !b;
+    /// <summary> Logical NOR (not a or not b</summary>
     public static bool Nor(this bool a, bool b) => !a || !b;
 
+    /// <summary> If the condition is true, execute the action </summary>
     public static void IfTrue(this bool condition, Action action) {
         if (condition) action();
     }
+    /// <summary> If the condition is true, execute the action </summary>
     public static void IfTrue<T>(this bool condition, Func<object> action){
         if (condition) action();
     }
 
+    /// <summary> If the condition is false, execute the action </summary>
     public static void IfFalse(this bool condition, Action action) {
         if (!condition) action();
     }
     
+    /// <summary> Select the action to perform based on the condition </summary>
     public static void Select(this bool condition, Action onTrue, Action onFalse) {
         if (condition) onTrue();
         else onFalse();
     }
     
+    /// <summary> Select the action to perform based on the condition </summary>
     public static void Select<T>(this bool condition, Action<T> onTrue, Action<T> onFalse, T arg) {
         if (condition) onTrue(arg);
         else onFalse(arg);
     }
 
+    /// <summary> Perform an action with passed arguments if true</summary>
     public static U IfTrue<T, U>(this bool condition, Func<T, U> action, T arg) => condition ? action(arg) : default;
 
+    /// <summary> Perform an action with passed arguments if false</summary>
     public static U IfFalse<T, U>(this bool condition, Func<T, U> action, T arg) => !condition ? action(arg) : default;
 
+    /// <summary> Perform an action with passed arguments if true</summary>
     public static void IfTrue<T>(this bool condition, Action<T> action, T arg) {
         if (condition) action(arg);
     }
+    /// <summary> Perform an action with passed arguments if false</summary>
     public static void IfFalse<T>(this bool condition, Action<T> action, T arg) {
         if (!condition) action(arg);
     }
