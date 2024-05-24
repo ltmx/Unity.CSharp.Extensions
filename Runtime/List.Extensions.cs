292b21f52967a51255c58fc1b1f06bd3ee62a57b
LTMX <47640688+LTMX@users.noreply.github.com>
2024-05-24 15:43:44 +0100

    Added to List.Extensions : ToItemList() & xml documentation

--- Runtime/List.Extensions.cs
+++ Runtime/List.Extensions.cs
@@ -6,15 +6,18 @@
 
 #endregion
 
-using System;
 using System.Collections.Generic;
 
 public static class ListExtensions
 {
+    /// <summary> Adds self item to the target list </summary>
     public static void AddTo<T>(this T item, List<T> list) => list.Add(item);
 
+    /// <summary> Adds self ienumerable to the target list </summary>
     public static void AddTo<T>(this IEnumerable<T> item, List<T> list) => list.AddRange(item);
-
-    public static List<string> ToList(this string item) => new() { item };
     
+    /// <summary> Creates a new list from a single item </summary>
+    public static List<T> ToItemList<T>(this T item) where T : struct => new() { item };
+    /// <summary> Creates a new list of strings from a single string </summary>
+    public static List<string> ToItemList(this string item) => new() { item };
 }
