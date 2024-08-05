383241b587632ef8f0ad2b68dc36f6fb4cdc97d3
Nicolas Reinhard <nicolasreinhard.pro@gmail.com>
2024-08-05 09:59:33 +0200

    Added null propagation check

--- Runtime/Condition.Extensions.cs
+++ Runtime/Condition.Extensions.cs
@@ -26,27 +26,27 @@ public static class ConditionExtensions {
 
     /// <summary> If the condition is true, execute the action </summary>
     public static void IfTrue(this bool condition, Action action) {
-        if (condition) action();
+        if (condition) action?.Invoke();
     }
     /// <summary> If the condition is true, execute the action </summary>
     public static void IfTrue<T>(this bool condition, Func<object> action){
-        if (condition) action();
+        if (condition) action?.Invoke();
     }
 
     /// <summary> If the condition is false, execute the action </summary>
     public static void IfFalse(this bool condition, Action action) {
-        if (!condition) action();
+        if (!condition) action?.Invoke();
     }
     
     /// <summary> Select the action to perform based on the condition </summary>
     public static void Select(this bool condition, Action onTrue, Action onFalse) {
-        if (condition) onTrue();
+        if (condition) onTrue?.Invoke();
         else onFalse();
     }
     
     /// <summary> Select the action to perform based on the condition </summary>
     public static void Select<T>(this bool condition, Action<T> onTrue, Action<T> onFalse, T arg) {
-        if (condition) onTrue(arg);
+        if (condition) onTrue?.Invoke(arg);
         else onFalse(arg);
     }
 
@@ -58,11 +58,11 @@ public static class ConditionExtensions {
 
     /// <summary> Perform an action with passed arguments if true</summary>
     public static void IfTrue<T>(this bool condition, Action<T> action, T arg) {
-        if (condition) action(arg);
+        if (condition) action?.Invoke(arg);
     }
     /// <summary> Perform an action with passed arguments if false</summary>
     public static void IfFalse<T>(this bool condition, Action<T> action, T arg) {
-        if (!condition) action(arg);
+        if (!condition) action?.Invoke(arg);
     }
     
     public static void flip(this ref bool condition) => condition = !condition;
